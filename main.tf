7355c749e3dc38938848ffc7f1b617a83e853be9
dragonknight320 <dragonknight320@gmail.com>
2021-07-01 18:13:51 +0700

    commit

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+//Ip for NAT gateway
+resource "aws_eip" "nat" {
+  # count = 3
+
+  vpc = true
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -8,11 +15,16 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway  = true
+  single_nat_gateway  = true
+  reuse_nat_ips       = true           # <= Skip creation of EIPs for the NAT Gateways
+  external_nat_ip_ids = "${aws_eip.nat.*.id}" # <= IPs specified here as input to the module
+
   enable_vpn_gateway = false
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
+
--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   # Configuration options
-  region = var.region
+  region                  = var.region
   shared_credentials_file = "./.aws/credentials"
 }
